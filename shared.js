9cc1c86f288e07711f141c9bf08fd8671dc37645
Alex Hawking <alexhawking23@gmail.com>
2023-09-18 20:06:55 +0800

    initial commit, add playing functionality

--- shared.js
+++ shared.js
@@ -2,71 +2,72 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
 let audioContext = new AudioContext();
 
-// Generates frequency to oscillate based on note and octave
-// https://gist.github.com/marcgg/94e97def0e8694f906443ed5262e9cbb?permalink_comment_id=3896533#gistcomment-3896533
-var NOTES = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B"];
-function getPitch(note, octave) {
-    var step = NOTES.indexOf(note);
-    var power = Math.pow(2, (octave * 12 + step - 57)/12);
-    var pitch = 440 * power;
-    return pitch;
-}
-
-function playInstrumentNote(instrument, note) {
-    // Get the base note and the octave so that I can get the frequency to oscillate
+// Number of semitones each note is above C
+const noteToSemitones = {
+	'C': 0,
+	'C#': 1,
+	'D': 2,
+	'D#': 3,
+	'E': 4,
+	'F': 5,
+	'F#': 6,
+	'G': 7,
+	'G#': 8,
+	'A': 9,
+	'A#': 10,
+	'B': 11
+  };
+  
+  // Calculate the number of semitones between two notes
+  function semitonesBetweenNotes(baseNote, targetNote) {
+	const baseSemitones = noteToSemitones[baseNote];
+	const targetSemitones = noteToSemitones[targetNote];
+	if (baseSemitones === undefined || targetSemitones === undefined) {
+	  throw new Error('Invalid note name');
+	}
+	return targetSemitones - baseSemitones;
+  }
+  
+  // Calculate pitch rate based on semitones and octaves using formula playbackRate = 2^(semitones/12 + octaves)
+  function calculatePitchRate(semitones, octaves = 0) {
+	return Math.pow(2, (semitones / 12) + octaves);
+  }
+  
+  // Load audio file
+  async function loadAudioFile(url) {
+	const response = await fetch(url);
+	const arrayBuffer = await response.arrayBuffer();
+	const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+	return audioBuffer;
+  }
+  
+  // Function to play audio buffer with pitch change
+  function playAudioBuffer(audioBuffer, pitchRate) {
+	const source = audioContext.createBufferSource();
+	source.buffer = audioBuffer;
+	source.playbackRate.value = pitchRate;
+	source.connect(audioContext.destination);
+	source.start();
+  }
+  
+  async function playInstrumentNote(instrument, note) {
 	let base = note[0].toUpperCase();
-	let frequency;
-	if (note[1]) {
-		frequency = getPitch(base + note[1], note[2])
+	let name;
+	if (note[1] === '#') {
+	  name = base + note[1];
 	} else {
-		frequency = getPitch(base, note[2])
+	  name = base;
 	}
-	let currentTime = audioContext.currentTime;
-
-	// Basic sound design for the 2 instruments
+	const semitones = semitonesBetweenNotes('C', name);
+	const octaves = note[2] - 3; 
+	const pitchRate = calculatePitchRate(semitones, octaves);
+	let audioBuffer;
 	if (instrument === 'piano') {
-		// Simple sine wave, with piano esque sound profile
-		let oscillator = audioContext.createOscillator();
-		oscillator.type = 'sine';
-		oscillator.frequency.setValueAtTime(frequency, currentTime);
-		// Use gain node to control volume at different times
-		let gainNode = audioContext.createGain();
-		gainNode.gain.setValueAtTime(0, currentTime);
-		gainNode.gain.linearRampToValueAtTime(1, currentTime + 0.01); 
-		gainNode.gain.exponentialRampToValueAtTime(0.01, currentTime + 1); 
-		oscillator.connect(gainNode);
-		gainNode.connect(audioContext.destination);
-		oscillator.start(currentTime);
-		oscillator.stop(currentTime + 1); 
+		audioBuffer = await loadAudioFile('c_piano.ogg');
 	} else if (instrument === 'violin') {
-		// Two oscillators, one sawtooth and one triangle, with different frequencies, to emulate violin esque sound
-		const oscillator1 = audioContext.createOscillator();
-		oscillator1.type = 'sawtooth';
-		oscillator1.frequency.setValueAtTime(frequency, currentTime);
-		const oscillator2 = audioContext.createOscillator();
-		oscillator2.type = 'triangle';
-		oscillator2.frequency.setValueAtTime(frequency * 2, currentTime); 
-		// Use gain nodes to control sound profile
-		const gainNode1 = audioContext.createGain();
-		const gainNode2 = audioContext.createGain();
-
-		gainNode1.gain.setValueAtTime(0, currentTime);
-		gainNode1.gain.linearRampToValueAtTime(0.4, currentTime + 0.1);  
-		gainNode1.gain.exponentialRampToValueAtTime(0.01, currentTime + 1);
-
-		gainNode2.gain.setValueAtTime(0, currentTime);
-		gainNode2.gain.linearRampToValueAtTime(0.2, currentTime + 0.1);  
-		gainNode2.gain.exponentialRampToValueAtTime(0.01, currentTime + 1); 
-		oscillator1.connect(gainNode1);
-		oscillator2.connect(gainNode2);
-
-		gainNode1.connect(audioContext.destination);
-		gainNode2.connect(audioContext.destination);
-		oscillator1.start(currentTime);
-		oscillator2.start(currentTime);
-		oscillator1.stop(currentTime + 1.5);
-		oscillator2.stop(currentTime + 1.5);
+		audioBuffer = await loadAudioFile('c_violin.ogg');
 	}
+	playAudioBuffer(audioBuffer, pitchRate);
   }
 
 const shared = () => {
